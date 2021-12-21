f755e40aebeafc7e751e09ae5bb63a22aebc79ba
danica22nd <96465040+danica22nd@users.noreply.github.com>
2021-12-21 22:45:16 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+$(".giftGif").click(musicPlay);
+
+
+var startMan = false;
+
+function musicPlay() {
+  $(".overlay").fadeOut();
+  if(!startMan){
+    var musicStart = new Audio("ikawLang.mp3");
+    musicStart.play();
+    startMan = true;
+    document.removeEventListener('click', musicPlay);
+    }
+}
+
+$(window).scroll(function() {
+    musicPlay();
+    $('.fade-in').each(function() {
+        var top_of_element = $(this).offset().top;
+        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+        var top_of_screen = $(window).scrollTop();
+
+        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
+            $(this).addClass('is-visible');
+        }
+    });
+});
